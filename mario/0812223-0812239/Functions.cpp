95fff9f5d0568fbc05c0cc316b4df62b8c26e718
minhkhanhhaugiang <minhkhanhhaugiang@8c528dec-4276-f233-101d-c843d64250d8>
2010-06-07 06:15:08 +0000

    edit intro  doc dc file tv

--- mario/0812223-0812239/Functions.cpp
+++ mario/0812223-0812239/Functions.cpp
@@ -38,3 +38,38 @@ int Functions::GetScreenCenterX()
 {
 	return SCREEN_WIDTH / 2;
 }
+
+wchar_t * Functions::UTF8_to_WChar(const char *string){
+	long b=0,
+		c=0;
+	if ((unsigned char)string[0]==BOM8A && (unsigned char)string[1]==BOM8B && (unsigned char)string[2]==BOM8C)
+		string+=3;
+	for (const char *a=string;*a;a++)
+		if (((unsigned char)*a)<128 || (*a&192)==192)
+			c++;
+	wchar_t *res=new wchar_t[c+1];
+	res[c]=0;
+	for (unsigned char *a=(unsigned char*)string;*a;a++){
+		if (!(*a&128))
+			//Byte represents an ASCII character. Direct copy will do.
+			res[b]=*a;
+		else if ((*a&192)==128)
+			//Byte is the middle of an encoded character. Ignore.
+			continue;
+		else if ((*a&224)==192)
+			//Byte represents the start of an encoded character in the range
+			//U+0080 to U+07FF
+			res[b]=((*a&31)<<6)|a[1]&63;
+		else if ((*a&240)==224)
+			//Byte represents the start of an encoded character in the range
+			//U+07FF to U+FFFF
+			res[b]=((*a&15)<<12)|((a[1]&63)<<6)|a[2]&63;
+		else if ((*a&248)==240){
+			//Byte represents the start of an encoded character beyond the
+			//U+FFFF limit of 16-bit integers
+			res[b]='?';
+		}
+		b++;
+	}
+	return res;
+}
